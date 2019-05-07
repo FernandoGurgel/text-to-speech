cc3346680e55a526283a597cbb68c3b652bcab9a
Fernando Gurgel <fag.gurgel@gmail.com>
2019-05-06 21:28:36 -0400

    commit init

---
+++ main.js
@@ -0,0 +1,13 @@
+var textToSpeech = require("text-to-speech-js");
+
+
+textToSpeech.talk("Hello Beautiful World!");
+// var voiceName = "voice_nick1"
+
+// say.speak("Luke, I am your father", voiceName, 0.75 , (err) => {
+//     if (err) {
+//         return console.error(err);
+//     }
+
+//     console.log(`Text with the voice ${voice}`);
+// });
